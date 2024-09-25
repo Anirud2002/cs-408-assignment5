9b45482d27af26bf0d1b5c740e55367a445cdecf
anirudshrestha <ANIRUDSHRESTHA@boisestate.edu>
2024-09-24 23:32:00 -0600

    almost done

--- js/main.js
+++ js/main.js
@@ -13,6 +13,16 @@ function loaded() {
  * This function returns the string 'hello'
  * @return {string} the string hello
  */
-export function sayHello() {
+ function sayHello() {
     return 'hello';
 }
+
+/**
+ * This function pops up alert with current radio button selected value
+ * @return {string} the string hello
+ */
+function showAlert() {
+    // Get the currently checked radio button
+    const selectedRadio = document.querySelector('input[name="sameValue"]:checked');
+    selectedRadio && alert(selectedRadio.value);
+  }
